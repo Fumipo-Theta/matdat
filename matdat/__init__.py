91404710980210c09a1dbaf42d196c952dac39e8
twined quartz <fmorisato@gmail.com>
2018-09-29 23:13:39 +0900

    export plotAction

--- matdat/__init__.py
+++ matdat/__init__.py
@@ -4,4 +4,4 @@ from .subplot_time import SubplotTime
 from .csv_reader import CsvReader
 from .get_path import getFileList
 from .save_plot import actionSavePNG
-from .plot_action import setGrid, scatterPlot, linePlot, bandPlot, barPlot, velocityPlot, velocityPlotRGB, vLinePlotRGB, movalMeanPlot
+from .plot_action import setGrid, scatterPlot, linePlot, bandPlot, barPlot, velocityPlot, velocityPlotRGB, vLinePlotRGB, movalMeanPlot, set_xlim, set_ylim, set_ylabel, setStyle
