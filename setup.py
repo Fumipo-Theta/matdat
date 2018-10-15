6fd9d306cd0bb7af98e68ccef6309d3ad6c4213f
twined quartz <fmorisato@gmail.com>
2018-10-15 16:40:14 +0900

    Update dependency

--- setup.py
+++ setup.py
@@ -13,16 +13,16 @@ with open('LICENSE') as f:
 
 setup(
     name='matdat',
-    version='1.0.0',
+    version='1.1.0',
     description='Separate data and layout in matplotlib. This provides data layer',
     long_description=readme,
     author='Fumitoshi Morisato',
     author_email='fmorisato@gmail.com',
     url='https://github.com/Fumipo-Theta/matdat',
     install_requires=[
-        'numpy', 'matplotlib', 'pandas', 'IPython', 'tqdm', 'chardet', 'matpos==1.0.0', 'func_helper==1.0.0'],
+        'numpy', 'matplotlib', 'pandas', 'IPython', 'tqdm', 'chardet', 'matpos==1.1.0', 'func_helper==1.0.0'],
     dependency_links=[
-        'git+https://github.com/Fumipo-Theta/matpos.git@master#egg=matpos-1.0.0', 'git+https://github.com/Fumipo-Theta/func_helper.git@master#egg=func_helper-1.0.0'],
+        'git+https://github.com/Fumipo-Theta/matpos.git@master#egg=matpos-1.1.0', 'git+https://github.com/Fumipo-Theta/func_helper.git@master#egg=func_helper-1.0.0'],
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
