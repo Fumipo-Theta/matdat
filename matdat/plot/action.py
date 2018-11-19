0afa600006e93e785a3d2eb36606c573e148d3ac
twined quartz <fmorisato@gmail.com>
2018-11-20 07:15:22 +0900

    modify

--- matdat/plot/action.py
+++ matdat/plot/action.py
@@ -1040,6 +1040,9 @@ def _factor_bar_plotter(
     legend={},
     **kwargs):
 
+    if type(y) is list:
+        return _bar_plotter(df,x,y,agg,*arg,xfactor=xfactor,norm=norm,vert=vert,legend=legend,**kwargs)
+
     """
     1. stacking bar plotのstackしていくgroupingをつくる
     """
@@ -1148,5 +1151,109 @@ def factor_bar(**presetting):
             "norm":False,
             "vert":True,
             "legend":{}
+        }
+    )(**presetting)
+
+
+def _bar_plotter(
+    df: pd.DataFrame,
+    x,  # factor1 selector
+    y: str,  # stack factor selector
+    agg,  # aggregate
+    *arg,
+    xfactor=None,  # explicit factor list
+    norm=False,
+    vert=True,
+    legend={},
+        **kwargs):
+
+    stack_factor = y if type(y) is list else [y]
+    stack_bars = []
+    for stack_name in stack_factor:
+        subset = df
+
+        x_factor_series, x_factor = Iget_factor(subset, x, xfactor)
+
+        x_group = subset.groupby(
+            pd.Categorical(
+                x_factor_series,
+                ordered=True,
+                categories=x_factor
+            )
+        )
+
+        subset_for_x_factor = [
+            subset.loc[x_group.groups[xfname]]
+            for xfname in x_factor
+        ]
+
+        stack_heights = pip(
+            it.mapping(lambda df: agg(df[stack_name])),
+            #it.mapping(lambda arr: arr[0] if len(arr) > 0 else 0),
+            list
+        )(subset_for_x_factor)
+
+        stack_bars.append(stack_heights)
+
+
+    if norm:
+        sum = pip(
+            it.mapping(np.sum),
+            list
+        )(zip(*stack_bars))
+
+        stack_bars = pip(
+            it.mapping(lambda bars: pip(
+                it.mapping(lambda t: 0 if t[1] in [
+                           0, None, np.nan] else t[0]/t[1]),
+                list
+            )(zip(bars, sum))),
+            list
+        )(stack_bars)
+
+    ind = list(range(len(x_factor)))
+
+    def plot(ax):
+        prev_top = stack_bars[0]
+        for i, bar in enumerate(stack_bars):
+            if vert:
+                if i is 0:
+                    ax.bar(ind, bar, **kwargs)
+                else:
+                    ax.bar(
+                        ind, bar, bottom=prev_top, **kwargs)
+                    prev_top = [a+b for a, b in zip(prev_top, bar)]
+            else:
+                if i is 0:
+                    ax.barh(ind, bar, **kwargs)
+                else:
+                    ax.barh(
+                        ind, bar, left=prev_top, **kwargs)
+                    prev_top = [a+b for a, b in zip(prev_top, bar)]
+
+        ax.legend(stack_factor, **legend)
+
+        if vert:
+            ax.set_xticks(ind)
+            ax.set_xticklabels(x_factor)
+            ax.set_xlim([-1, len(x_factor)])
+        else:
+            ax.set_yticks(ind)
+            ax.set_yticklabels(x_factor)
+            ax.set_ylim([-1, len(x_factor)])
+        return ax
+    return plot
+
+
+def bar(**presetting):
+    return plot_action(
+        _bar_plotter,
+        generate_arg_and_kwags(get_value()),
+        ["x", "y", "agg"],
+        {
+            "xfactor": None,
+            "norm": False,
+            "vert": True,
+            "legend": {}
         }
     )(**presetting)
