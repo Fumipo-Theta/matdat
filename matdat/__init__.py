0f143dc3c9a069520529fa814c9d92dc5535d4d3
twined quartz <fmorisato@gmail.com>
2019-06-01 21:11:20 +0900

    update

--- matdat/__init__.py
+++ matdat/__init__.py
@@ -5,3 +5,8 @@ from .csv_reader import CsvReader, matchCsv
 from .excel_reader import ExcelReader
 from .get_path import getFileList, PathList, multiple_path_finder
 from .save_plot import actionSavePNG
+
+
+print("""MatDat is deprecated.
+Please migrate into 'structured_plot' and 'data_loader' package
+""")
