51973822235154180618dbcd369b375ec7254ece
twined quartz <fmorisato@gmail.com>
2019-06-02 17:02:48 +0900

    hide provate function

--- matdat/save_plot.py
+++ matdat/save_plot.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 
 
-def safe_filename(filename: str) -> str:
+def __safe_filename(filename: str) -> str:
     return filename.replace("/", "").replace("\\", "")
 
 
@@ -10,4 +10,4 @@ def actionSavePNG(directory, filename):
     # パスが有効かどうかを検証し, ディレクトリがなければ作成する
     if not os.path.isdir(directory):
         os.makedirs(directory)
-    return lambda postfix="": plt.savefig(directory+safe_filename(filename+postfix+'.png'))
+    return lambda postfix="": plt.savefig(directory+__safe_filename(filename+postfix+'.png'))
